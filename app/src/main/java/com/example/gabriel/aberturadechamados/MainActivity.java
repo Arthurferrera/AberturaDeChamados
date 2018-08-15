14dfacf0e4def0b872bab0d9a8ae4b68b54475d7
Guilherme <teste@teste.com>
2018-08-15 11:43:46 -0300

    continuação front-end

--- app/src/main/java/com/example/gabriel/aberturadechamados/MainActivity.java
+++ app/src/main/java/com/example/gabriel/aberturadechamados/MainActivity.java
@@ -3,23 +3,31 @@ package com.example.gabriel.aberturadechamados;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+    FloatingActionButton fab;
+    ListView list_view_chamados;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        finds dos elementos
+        fab = findViewById(R.id.fab);
+        list_view_chamados = findViewById(R.id.list_view_chamados);
+
+//        ação do botão float
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -27,5 +35,10 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intencao);
             }
         });
+    }
+
+    public void VisualizarChamado(View view) {
+        Intent intencao = new Intent(getApplicationContext(), VisualizarChamadoActivity.class);
+        startActivity(intencao);
     }
 }
