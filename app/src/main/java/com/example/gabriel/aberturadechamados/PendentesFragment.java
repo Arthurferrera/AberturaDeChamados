b2e8d66d1484471638964318a76754cf11b15338
Guilherme <teste@teste.com>
2018-09-26 11:42:23 -0300

    inicio de fragments

--- app/src/main/java/com/example/gabriel/aberturadechamados/PendentesFragment.java
+++ app/src/main/java/com/example/gabriel/aberturadechamados/PendentesFragment.java
@@ -1,33 +1,33 @@
 package com.example.gabriel.aberturadechamados;
 
+
 import android.app.Activity;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentTransaction;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.v4.app.FragmentManager;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AlertDialog;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class PendentesFragment extends Fragment implements AdapterView.OnItemClickListener {
 
     ListView list_view_chamados;
     ChamadoAdapter adapter;
@@ -35,44 +35,48 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     int idUsuario;
     private SharedPreferencesConfig preferencesConfig;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+    public PendentesFragment() {
+        // Required empty public constructor
+    }
 
-//        Toast.makeText(this, "Bem Vindo !", Toast.LENGTH_SHORT);
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_pendentes, container, false);
 
         API_URL = getString(R.string.api_key);
 
 //        instanciando o SharedPreferencesConfig
-        preferencesConfig = new SharedPreferencesConfig(getApplicationContext());
+        preferencesConfig = new SharedPreferencesConfig(getActivity());
 
 //        finds dos elementos
-        FloatingActionButton fab = findViewById(R.id.fab);
-        list_view_chamados = findViewById(R.id.list_view_chamados);
+        FloatingActionButton fab = view.findViewById(R.id.fab);
+        list_view_chamados = view.findViewById(R.id.list_view_chamados);
 
 //        setando o adapter  e o click do item da lista
-        adapter = new ChamadoAdapter(this);
+        adapter = new ChamadoAdapter(getActivity());
         list_view_chamados.setAdapter(adapter);
-        list_view_chamados.setOnItemClickListener(this);
+//        list_view_chamados.setOnItemClickListener();
 
-//        ação do botão float
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intencao = new Intent(getApplicationContext(), AbrirChamadoActivity.class);
-                startActivity(intencao);
-            }
-        });
+////        ação do botão float
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intencao = new Intent(getActivity(), AbrirChamadoActivity.class);
+//                startActivity(intencao);
+//            }
+//        });
+
+        return view;
     }
 
     @Override
-    protected void onResume() {
+    public void onResume() {
         super.onResume();
 
-//        pegando as informações do usuario gravados no celular
+        //        pegando as informações do usuario gravados no celular
         nomeUsuario = preferencesConfig.readUsuarioNome();
         idUsuario = Integer.parseInt(preferencesConfig.readUsuarioId());
 
@@ -127,52 +131,14 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         }.execute();
     }
 
-//    método que seta o click de um item da lista
+    //    método que seta o click de um item da lista
 //    chama a tela de visualização, passando o id do item
     @Override
     public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
         Chamado item = adapter.getItem(i);
-        Intent intencao = new Intent(this, VisualizarChamadoActivity.class);
+        Intent intencao = new Intent(getActivity(), VisualizarChamadoActivity.class);
         intencao.putExtra("idChamado", item.getId());
         startActivity(intencao);
-    }
-
-//    método que faz o logoff do aplicativo
-//    mudando o stats do login, para falso
-    public void Sair() {
-        preferencesConfig.writeLoginStatus(false);
-        startActivity(new Intent(this, LoginActivity.class));
-        finish();
-    }
-
-//    cria o menu superior
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu, menu);
-        return super.onCreateOptionsMenu(menu);
-    }
-
-//    setando as opções do menu
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        if(id == R.id.action_sair){
-            AlertDialog.Builder builder = new AlertDialog.Builder(this);
-            builder.setTitle("Sair");
-            builder.setMessage("Tem certeza que deseja sair do aplicativo?");
-            final Activity activity = this;
-            builder.setPositiveButton("SIM", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialogInterface, int i) {
-                    Sair();
-                }
-            });
-            builder.setNegativeButton("NÃO", null);
-            builder.create().show();
-
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
     }
 
     public String converterData(String dataTotal){
